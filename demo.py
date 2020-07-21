f9e5fe1898ca2bdd062f20b8b48f9bbb2a940c35
pjrule <parker.rule@tufts.edu>
2020-07-21 16:15:54 -0400

    Update demo to use post-refactor API

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import json
 from dateutil.parser import parse as ts_parse
 from covid_scheduling import (
-    validate_people, validate_config, bipartite_assign
+    validate_people, validate_config, assign_schedules
 )
 
 if __name__ == '__main__':
@@ -9,7 +9,7 @@ if __name__ == '__main__':
         config = validate_config(json.load(f)['campuses'])
     with open('data/tufts_sample_5000.json') as f:
         people = validate_people(json.load(f), config)
-    print(bipartite_assign(config['Medford/Somerville'],
-                           people[:300],
+    print(assign_schedules(config,
+                           people,
                            ts_parse('2020-08-17'),
                            ts_parse('2020-08-23')))
